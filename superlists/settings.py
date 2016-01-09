8ea15d82f559bd306396ca32a9636df3d2e6711f
AbrahamV <abraham.varricatt@googlemail.com>
2016-01-09 17:34:11 +0530

    disabling LOC count

--- superlists/settings.py
+++ superlists/settings.py
@@ -91,6 +91,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 JENKINS_TASKS = (
         'django_jenkins.tasks.run_pylint',
         'django_jenkins.tasks.run_pyflakes',
-        'django_jenkins.tasks.run_sloccount',
 )
 
